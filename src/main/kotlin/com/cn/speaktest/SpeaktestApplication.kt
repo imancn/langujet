d76be9d3fa93714d15add3a299c35a0b37ef032d
Iman Soltani <iman.soltani.j@gmail.com>
2022-05-01 12:26:48 +0430

    Initial Commit

---
+++ src/main/kotlin/com/cn/speaktest/SpeaktestApplication.kt
@@ -0,0 +1,11 @@
+package com.cn.speaktest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpeaktestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpeaktestApplication>(*args)
+}
---
+++ src/test/kotlin/com/cn/speaktest/SpeaktestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cn.speaktest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpeaktestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
