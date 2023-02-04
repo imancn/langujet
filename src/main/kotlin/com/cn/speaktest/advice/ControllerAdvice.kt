089f929536f2821df694a2ddb3149a2f322dcabb
Iman Soltani <iman.soltani.j@gmail.com>
2023-02-05 02:38:52 +0330

    Handle validation exceptions

--- src/main/kotlin/com/cn/speaktest/advice/ControllerAdvice.kt
+++ src/main/kotlin/com/cn/speaktest/advice/ControllerAdvice.kt
@@ -1,14 +1,17 @@
 package com.cn.speaktest.advice
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
+import javax.validation.ValidationException
 
 @RestControllerAdvice
 class ControllerAdvice {
     @ExceptionHandler(value = [HttpException::class])
-    fun handleTokenRefreshException(ex: HttpException, request: WebRequest): ResponseEntity<Message> {
+    fun handleHttpException(ex: HttpException, request: WebRequest): ResponseEntity<Message> {
         return ResponseEntity(
             Message(
                 ex.httpStatus,
@@ -16,6 +19,29 @@ class ControllerAdvice {
                 ex.stackTraceToString()
             ),
             ex.httpStatus
+        )
+    }
+    @ExceptionHandler(value = [MethodArgumentNotValidException::class])
+    fun handleValidationException(ex: MethodArgumentNotValidException, request: WebRequest): ResponseEntity<Message> {
+        return ResponseEntity(
+            Message(
+                HttpStatus.NOT_ACCEPTABLE,
+                ex.message,
+                ex.stackTraceToString()
+            ),
+            HttpStatus.NOT_ACCEPTABLE
+        )
+    }
+
+    @ExceptionHandler(value = [ValidationException::class])
+    fun handleValidationException(ex: ValidationException, request: WebRequest): ResponseEntity<Message> {
+        return ResponseEntity(
+            Message(
+                HttpStatus.NOT_ACCEPTABLE,
+                ex.message,
+                ex.stackTraceToString()
+            ),
+            HttpStatus.NOT_ACCEPTABLE
         )
     }
 }
