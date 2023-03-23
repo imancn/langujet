e6fcd6691f584373b593d11b07d2e243eb9cc7e5
Iman Soltani <iman.soltani.j@gmail.com>
2023-03-23 21:32:54 +0430

    Extract AdminService

--- src/main/kotlin/com/cn/speaktest/exam/service/ExamServiceImpl.kt
+++ src/main/kotlin/com/cn/speaktest/exam/service/ExamServiceImpl.kt
@@ -1,17 +1,24 @@
 package com.cn.speaktest.exam.service
 
 import com.cn.speaktest.exam.api.ExamService
+import com.cn.speaktest.exam.model.Exam
 import com.cn.speaktest.exam.model.ExamIssue
+import com.cn.speaktest.exam.model.ExamRequest
 import com.cn.speaktest.exam.model.Question
 import com.cn.speaktest.exam.repository.ExamIssueRepository
+import com.cn.speaktest.exam.repository.ExamRepository
+import com.cn.speaktest.exam.repository.ExamRequestRepository
 import com.cn.speaktest.exam.repository.QuestionRepository
+import com.cn.speaktest.professor.Professor
 import org.springframework.stereotype.Service
 
 @Service
 class ExamServiceImpl(
+    val examRequestRepository: ExamRequestRepository,
+    val examRepository: ExamRepository,
     val questionRepository: QuestionRepository,
-    val examIssueRepository: ExamIssueRepository
-): ExamService {
+    val examIssueRepository: ExamIssueRepository,
+) : ExamService {
     override fun generateExamIssueList(examId: String): List<ExamIssue> {
         val examIssues = mutableListOf<ExamIssue>().toMutableList()
         val usedQuestions = mutableListOf<Question>().toMutableList()
@@ -23,21 +30,35 @@ class ExamServiceImpl(
                 examIssues.add(
                     examIssueRepository.save(
                         ExamIssue(
-                            examId,
-                            question,
-                            examIssues.size
+                            examId, question, examIssues.size
                         )
                     )
                 )
             }
             usedQuestions.addAll(questions)
         }
-        questionRepository.saveAll(
-            usedQuestions.onEach {
-                it.usageNumber = it.usageNumber + 1
-            }
-        )
+        questionRepository.saveAll(usedQuestions.onEach {
+            it.usageNumber = it.usageNumber + 1
+        })
         return examIssues
+    }
+
+    override fun submitExam(
+        examRequest: ExamRequest, professor: Professor
+    ): Exam {
+        var exam = examRepository.save(
+            Exam(
+                examRequest.student, professor, examRequest.date
+            )
+        )
+
+        val examIssues = generateExamIssueList(
+            exam.id ?: throw RuntimeException("exam.id must not be null")
+        )
+
+        exam = examRepository.save(exam.also { it.examIssues = examIssues })
+        examRequestRepository.delete(examRequest)
+        return exam
     }
 
 }
