f35643b2f0bf5dc3eefae50b73914e008e6a24d1
Iman Soltani <iman.soltani.j@gmail.com>
2023-03-23 20:48:57 +0430

    Modify StudentProfileResponse

--- src/main/kotlin/com/cn/speaktest/student/payload/response/StudentProfileResponse.kt
+++ src/main/kotlin/com/cn/speaktest/student/payload/response/StudentProfileResponse.kt
@@ -3,14 +3,12 @@ package com.cn.speaktest.student.payload.response
 import com.cn.speaktest.student.model.Student
 
 data class StudentProfileResponse(
-    var id: String?,
     var email: String,
     var fullName: String,
     var biography: String?,
     var credit: Double = 0.0,
 ) {
     constructor(student: Student) : this(
-        id = student.id,
         email = student.user.email,
         fullName = student.fullName,
         biography = student.biography,
