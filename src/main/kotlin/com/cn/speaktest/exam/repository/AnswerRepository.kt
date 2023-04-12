d1ab8fc414c6e1027e8582a4c6bd2ee991f11bc6
Iman Soltani <iman.soltani.j@gmail.com>
2023-04-12 22:49:08 +0330

    Remove old AnswerRepository.kt

--- src/main/kotlin/com/cn/speaktest/exam/repository/AnswerRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.cn.speaktest.exam.repository
-
-import com.cn.speaktest.exam.model.Answer
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface AnswerRepository : MongoRepository<Answer, String>
