9f2ddfbc94cdf47695721e3d5db505115df9abd8
Iman Soltani <iman.soltani.j@gmail.com>
2023-05-25 19:13:11 +0330

    feat(File): kafka producer

---
+++ src/main/kotlin/com/cn/speaktest/application/kafka/producer/file/FileProducer.kt
@@ -0,0 +1,24 @@
+package com.cn.speaktest.application.kafka.producer.file
+
+import com.cn.speaktest.application.file.data.model.File
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class FileProducer(
+    private val kafkaTemplate: KafkaTemplate<String, String>,
+    private val mapper: ObjectMapper
+) {
+
+    @Value("\${spring.kafka.topics.file}")
+    private lateinit var fileTopic: String
+    fun sendFile(file: File) {
+        kafkaTemplate.send(
+            fileTopic,
+            file.name,
+            mapper.writeValueAsString(file))
+    }
+
+}
