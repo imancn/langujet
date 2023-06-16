3d483f765694d291d0c8f247f7e31f4fa54136eb
Iman Soltani <iman.soltani.j@gmail.com>
2023-06-16 12:54:36 +0330

    feat(HttpUtil): Use ResponseEntity instead of Message to resolve StudentController dependencies

--- src/main/kotlin/com/cn/langujet/actor/student/api/StudentController.kt
+++ src/main/kotlin/com/cn/langujet/actor/student/api/StudentController.kt
@@ -1,9 +1,9 @@
 package com.cn.langujet.actor.student.api
 
 import com.cn.langujet.actor.student.payload.response.StudentProfileResponse
-import com.cn.langujet.application.advice.Message
-import com.cn.langujet.application.advice.toOkMessage
+import com.cn.langujet.actor.util.toOkResponseEntity
 import com.cn.langujet.domain.student.service.StudentService
+import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
@@ -18,9 +18,10 @@ class StudentController(
 
     @GetMapping("/profile")
     @PreAuthorize("hasRole('STUDENT')")
-    fun getProfile(@RequestHeader("Authorization") auth: String?): Message {
-        return StudentProfileResponse(studentService.getStudentByAuthToken(auth)).toOkMessage()
-    }
+    fun getProfile(
+        @RequestHeader("Authorization") auth: String?
+    ): ResponseEntity<StudentProfileResponse> =
+        toOkResponseEntity(StudentProfileResponse(studentService.getStudentByAuthToken(auth)))
 
     @PutMapping("/profile")
     @PreAuthorize("hasRole('STUDENT')")
@@ -28,17 +29,14 @@ class StudentController(
         @RequestHeader("Authorization") auth: String?,
         @RequestParam fullName: String?,
         @RequestParam biography: String?,
-    ): Message {
-        return studentService.editProfile(auth, fullName, biography).toOkMessage()
-    }
+    ): ResponseEntity<StudentProfileResponse> =
+        toOkResponseEntity(studentService.editProfile(auth, fullName, biography))
 
     @PostMapping("/exam-request")
     @PreAuthorize("hasRole('STUDENT')")
     fun examRequest(
         @RequestParam examId: String,
         @RequestHeader("Authorization") auth: String?
-    ): Message {
-        studentService.createExamRequest(auth, examId)
-        return Message(null, "Your exam request submitted successfully")
-    }
+    ): ResponseEntity<String> =
+        toOkResponseEntity("Your exam request submitted successfully")
 }
