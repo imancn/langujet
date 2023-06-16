1df6ccffdbf65ae0cf4fbc8ebd4ebfe30331a9cc
Iman Soltani <iman.soltani.j@gmail.com>
2023-06-16 14:19:50 +0330

    Refactor(actor): Use ResponseEntity instead of Message to resolve AnswerController dependencies

--- src/main/kotlin/com/cn/langujet/actor/answer/api/AnswerController.kt
+++ src/main/kotlin/com/cn/langujet/actor/answer/api/AnswerController.kt
@@ -4,9 +4,10 @@ import com.cn.langujet.actor.answer.payload.request.ChoiceAnswerRequest
 import com.cn.langujet.actor.answer.payload.request.TextAnswerRequest
 import com.cn.langujet.actor.answer.payload.request.TrueFalseAnswerRequest
 import com.cn.langujet.actor.answer.payload.request.VoiceAnswerRequest
-import com.cn.langujet.application.advice.Message
-import com.cn.langujet.application.advice.toOkMessage
+import com.cn.langujet.actor.util.toOkResponseEntity
 import com.cn.langujet.domain.answer.AnswerService
+import com.cn.langujet.domain.answer.model.Answer
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 
@@ -19,32 +20,26 @@ class AnswerController(
     private val SUBMIT_MESSAGE = "Answer Submitted"
 
     @GetMapping("/{examIssueId}")
-    fun getAnswersByExamIssueId(@PathVariable examIssueId: String): Message {
-        return answerService.getAnswersByExamIssueId(examIssueId).toOkMessage()
-    }
+    fun getAnswersByExamIssueId(@PathVariable examIssueId: String): List<Answer> =
+        answerService.getAnswersByExamIssueId(examIssueId)
 
     @GetMapping("/{id}")
-    fun getAnswerById(@PathVariable id: String): Message {
-        return answerService.getAnswerById(id).toOkMessage()
-    }
+    fun getAnswerById(@PathVariable id: String): ResponseEntity<Answer> =
+        toOkResponseEntity(answerService.getAnswerById(id))
 
     @PostMapping("/text")
-    fun submitTextAnswer(@RequestBody answerRequest: TextAnswerRequest): Message {
-        return answerService.submitTextAnswer(answerRequest).toOkMessage(SUBMIT_MESSAGE)
-    }
+    fun submitTextAnswer(@RequestBody answerRequest: TextAnswerRequest): ResponseEntity<Answer.Text> =
+        toOkResponseEntity( answerService.submitTextAnswer(answerRequest))
 
     @PostMapping("/choice")
-    fun submitChoiceAnswer(@RequestBody answerRequest: ChoiceAnswerRequest): Message {
-        return answerService.submitChoiceAnswer(answerRequest).toOkMessage(SUBMIT_MESSAGE)
-    }
+    fun submitChoiceAnswer(@RequestBody answerRequest: ChoiceAnswerRequest): ResponseEntity<Answer.Choice> =
+        toOkResponseEntity(answerService.submitChoiceAnswer(answerRequest))
 
     @PostMapping("/true-false")
-    fun submitTrueFalseAnswer(@RequestBody answerRequest: TrueFalseAnswerRequest): Message {
-        return answerService.submitTrueFalseAnswer(answerRequest).toOkMessage(SUBMIT_MESSAGE)
-    }
+    fun submitTrueFalseAnswer(@RequestBody answerRequest: TrueFalseAnswerRequest): ResponseEntity<Answer.TrueFalse> =
+        toOkResponseEntity(answerService.submitTrueFalseAnswer(answerRequest))
 
     @PostMapping("/voice")
-    fun submitVoiceAnswer(@RequestBody answerRequest: VoiceAnswerRequest): Message {
-        return answerService.submitVoiceAnswer(answerRequest).toOkMessage(SUBMIT_MESSAGE)
-    }
+    fun submitVoiceAnswer(@RequestBody answerRequest: VoiceAnswerRequest): ResponseEntity<Answer.Voice> =
+        toOkResponseEntity(answerService.submitVoiceAnswer(answerRequest))
 }
