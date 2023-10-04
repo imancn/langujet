03ecdccd443aabd28103c718aac1d51a6dff35db
Iman Soltani <iman.soltani.j@gmail.com>
2023-10-04 23:07:40 +0330

    Chore(ExamIssue): Remove

--- src/main/kotlin/com/cn/langujet/domain/exam/repository/ExamIssueRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.cn.langujet.domain.exam.repository
-
-import com.cn.langujet.domain.exam.model.ExamIssue
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface ExamIssueRepository : MongoRepository<ExamIssue, String> {
-    fun findByAnswerId(id: String): ExamIssue?
-}
