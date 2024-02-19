ac1f7ec6d815fed3248ebaa1db74ec1c6135d002
Iman Soltani <iman.soltani.j@gmail.com>
2024-02-19 22:18:28 +0330

    Chore(OpenAPIConfiguration): Modify

--- src/main/kotlin/com/cn/langujet/application/config/OpenAPIConfiguration.kt
+++ src/main/kotlin/com/cn/langujet/application/config/OpenAPIConfiguration.kt
@@ -1,9 +1,13 @@
 package com.cn.langujet.application.config
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.info.License
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.annotations.servers.Server
 
 @OpenAPIDefinition(
@@ -19,6 +23,14 @@ import io.swagger.v3.oas.annotations.servers.Server
             name = "All rights reserved for Langujet Co.",
             url = "https://langujet.com")
     ),
-    servers = [Server(url = "/")]
+    servers = [Server(url = "/")],
+    security = [SecurityRequirement(name = "bearerAuth")]
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT",
+    `in` = SecuritySchemeIn.HEADER
 )
 class OpenAPIConfiguration
