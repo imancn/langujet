ea3e34ab91bc51ec8538c9b862fadcd800db68e7
Iman Soltani <iman.soltani.j@gmail.com>
2024-02-22 19:51:06 +0330

    Chore(SectionController): Change response to DTO

--- src/main/kotlin/com/cn/langujet/actor/section/api/SectionController.kt
+++ src/main/kotlin/com/cn/langujet/actor/section/api/SectionController.kt
@@ -1,15 +1,12 @@
 package com.cn.langujet.actor.section.api
 
 import com.cn.langujet.actor.exam.payload.SectionDTO
-import com.cn.langujet.actor.util.toCreatedResponseEntity
 import com.cn.langujet.actor.util.toOkResponseEntity
 import com.cn.langujet.application.advice.NotFoundException
-import com.cn.langujet.domain.exam.model.Section
 import com.cn.langujet.domain.exam.service.SectionService
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
-import java.net.URI
 
 @RestController
 @RequestMapping("api/v1/admin/section")
@@ -19,30 +16,27 @@ class SectionController(private val sectionService: SectionService) {
     @PreAuthorize("hasRole('ADMIN')")
     fun getSectionById(
         @PathVariable id: String
-    ): ResponseEntity<Section> = toOkResponseEntity(sectionService.getSectionById(id))
+    ): SectionDTO = SectionDTO(sectionService.getSectionById(id))
 
     @GetMapping("by-exam-id/{examId}")
     @PreAuthorize("hasRole('ADMIN')")
     fun getSectionByExamId(
         @PathVariable examId: String
-    ): ResponseEntity<List<Section>> =
-        toOkResponseEntity(sectionService.getSectionByExamId(examId))
+    ): List<SectionDTO> =
+        sectionService.getSectionByExamId(examId).map { SectionDTO(it) }
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     fun createSection(
         @RequestBody section: SectionDTO
-    ): ResponseEntity<Section> = sectionService.createSection(section.toSection()).let {
-        toCreatedResponseEntity(it, URI("/sections/${it.id}"))
-    }
+    ): SectionDTO = SectionDTO(sectionService.createSection(section.toSection()))
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     fun updateSection(
         @PathVariable id: String,
         @RequestBody section: SectionDTO
-    ): ResponseEntity<Section> =
-        toOkResponseEntity(sectionService.updateSection(id, section.toSection()))
+    ): SectionDTO = SectionDTO(sectionService.updateSection(id, section.toSection()))
 
     @DeleteMapping("/{id}")
      @PreAuthorize("hasRole('ADMIN')")
