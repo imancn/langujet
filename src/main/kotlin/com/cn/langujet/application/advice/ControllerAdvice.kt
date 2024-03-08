0720984a6ef0fa42d1704fe1585a47ed77518231
Iman Soltani <iman.soltani.j@gmail.com>
2024-03-09 00:51:52 +0330

    Chore(ControllerAdvice): Add HttpMessageConversionException handler

--- src/main/kotlin/com/cn/langujet/application/advice/ControllerAdvice.kt
+++ src/main/kotlin/com/cn/langujet/application/advice/ControllerAdvice.kt
@@ -4,6 +4,8 @@ import jakarta.validation.ValidationException
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageConversionException
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -12,7 +14,7 @@ import org.springframework.web.context.request.WebRequest
 @RestControllerAdvice
 class ControllerAdvice {
     private val logger = LoggerFactory.getLogger(javaClass.simpleName)
-
+    
     @ExceptionHandler(value = [HttpException::class])
     fun handleHttpException(ex: HttpException, request: WebRequest): ResponseEntity<String> {
         logger.error(ex.stackTraceToString())
@@ -21,7 +23,7 @@ class ControllerAdvice {
             ex.httpStatus
         )
     }
-
+    
     @ExceptionHandler(value = [MethodArgumentNotValidException::class])
     fun handleValidationException(ex: MethodArgumentNotValidException, request: WebRequest): ResponseEntity<String> {
         logger.error(ex.stackTraceToString())
@@ -30,10 +32,19 @@ class ControllerAdvice {
             HttpStatus.BAD_REQUEST
         )
     }
-
+    
     @ExceptionHandler(value = [ValidationException::class])
     fun handleValidationException(ex: ValidationException, request: WebRequest): ResponseEntity<String> {
         logger.error(ex.stackTraceToString())
+        return ResponseEntity(
+            ex.message,
+            HttpStatus.BAD_REQUEST
+        )
+    }
+    
+    // Todo: Remove after test
+    @ExceptionHandler(value = [HttpMessageConversionException::class])
+    fun handleHttpMessageConversionException(ex: HttpMessageNotReadableException, request: WebRequest): ResponseEntity<String> {
         return ResponseEntity(
             ex.message,
             HttpStatus.BAD_REQUEST
