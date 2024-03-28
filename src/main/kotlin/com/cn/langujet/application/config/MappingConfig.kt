a85f2d74daf393c6e406e7166b25a679eec5fb2a
Iman Soltani <iman.soltani.j@gmail.com>
2024-03-28 08:02:21 +0330

    feat(MappingConfig): Remove unnecessary config

--- src/main/kotlin/com/cn/langujet/application/config/MappingConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.cn.langujet.application.config
-
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-class MappingConfig { @Bean fun objectMapper() = ObjectMapper() }
