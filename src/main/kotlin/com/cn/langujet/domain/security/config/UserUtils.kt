909fa2e59617bdba84e1ea1ca546c05b16e3cb70
Iman Soltani <iman.soltani.j@gmail.com>
2024-04-02 07:47:33 +0330

    Feat(UserUtils): Implement

---
+++ src/main/kotlin/com/cn/langujet/domain/security/config/UserUtils.kt
@@ -0,0 +1,34 @@
+package com.cn.langujet.domain.security.config
+
+import com.cn.langujet.domain.security.model.UserDetailsImpl
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+
+object UserUtils {
+    fun getCurrentUserId(): String? {
+        val userDetails = getCurrentUserDetails() as? UserDetailsImpl
+        return userDetails?.id
+    }
+    
+    fun getCurrentUserEmail(): String? {
+        val userDetails = getCurrentUserDetails() as? UserDetailsImpl
+        return userDetails?.email
+    }
+    
+    fun isAdmin() = hasAuthority("ROLE_PROFESSOR")
+    
+    fun isProfessor() = hasAuthority("ROLE_PROFESSOR")
+    
+    fun isStudent() = hasAuthority("ROLE_STUDENT")
+    
+    private fun getCurrentUserDetails(): UserDetails? {
+        val authentication = SecurityContextHolder.getContext().authentication
+        return authentication?.principal as? UserDetails
+    }
+    
+    private fun hasAuthority(role: String): Boolean {
+        val userDetails = getCurrentUserDetails() as? UserDetailsImpl
+        return userDetails?.authorities?.contains(SimpleGrantedAuthority(role)) ?: false
+    }
+}
