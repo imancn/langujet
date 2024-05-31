62c466d4cb57845f27e79d58efa6b0b7bd5f927e
Iman Soltani <iman.soltani.j@gmail.com>
2024-06-01 00:06:58 +0330

    Add spring-cloud-starter-openfeign dependency

--- src/main/kotlin/com/cn/langujet/LangujetApplication.kt
+++ src/main/kotlin/com/cn/langujet/LangujetApplication.kt
@@ -2,10 +2,12 @@ package com.cn.langujet
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableFeignClients
 class LangujetApplication
 
 fun main(args: Array<String>) {
