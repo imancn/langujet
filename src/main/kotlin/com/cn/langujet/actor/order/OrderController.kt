5136229d7e2d4d54a1b97657b6cb394cc73df955
Iman Soltani <iman.soltani.j@gmail.com>
2024-06-17 04:21:01 +0330

    Remove Redirect to App test APIs

--- src/main/kotlin/com/cn/langujet/actor/order/OrderController.kt
+++ src/main/kotlin/com/cn/langujet/actor/order/OrderController.kt
@@ -4,7 +4,6 @@ import com.cn.langujet.actor.order.payload.*
 import com.cn.langujet.actor.order.payload.Currency
 import com.cn.langujet.actor.util.Auth
 import com.cn.langujet.domain.coupon.CouponService
-import com.cn.langujet.domain.order.UserDeviceType
 import com.cn.langujet.domain.order.service.OrderService
 import com.cn.langujet.domain.payment.model.PaymentType
 import com.cn.langujet.domain.region.ClientRegionService
@@ -13,8 +12,6 @@ import jakarta.validation.Valid
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.servlet.view.RedirectView
-import java.util.*
 
 
 @RestController
@@ -80,45 +77,5 @@ class OrderController(
             )
         }
         return paymentMethodList
-    }
-    
-    @GetMapping("redirect/test/js")
-    fun redirectTest(@RequestParam userDeviceType: UserDeviceType): String {
-        val status = "successful"
-        val id = UUID.randomUUID().toString()
-        val amount = 20
-        return "<!DOCTYPE html>\n" +
-            "<html lang=\"en\">\n" +
-            "<head>\n" +
-            "    <meta charset=\"UTF-8\">\n" +
-            "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-            "    <title>Redirect to App</title>\n" +
-            "</head>\n" +
-            "<body>\n" +
-            "    <script type=\"text/javascript\">\n" +
-            "        // Deep link structure\n" +
-            "        var status = \"failure\";\n" +
-            "        var id = \"331a7089-2dfd-4a89-9fc9-8603398a6a22\";\n" +
-            "        var amount = 200;\n" +
-            "        var deepLinkUrl = `com.gorilsoft.langujet://launch/payment?status=${status}&id=${id}&amount=${amount}`;\n" +
-            "\n" +
-            "        // Redirect to the deep link\n" +
-            "        window.location = deepLinkUrl;\n" +
-            "    </script>\n" +
-            "</body>\n" +
-            "</html>"
-    }
-    
-    @GetMapping("redirect/test/redirect-view")
-    fun redirectTestv2(@RequestParam userDeviceType: UserDeviceType): RedirectView {
-        val status = "successful"
-        val id = UUID.randomUUID().toString()
-        val amount = 20
-        val deepLinkUrl =
-            String.format("com.gorilsoft.langujet://launch/payment?status=%s&id=%s&amount=%d", status, id, amount)
-        
-        val redirectView = RedirectView()
-        redirectView.url = deepLinkUrl
-        return redirectView
     }
 }
