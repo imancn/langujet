df6d83e8086c9d8971c88f6d2091930ad9d0e986
Iman Soltani <iman.soltani.j@gmail.com>
2024-07-05 16:24:19 +0330

    Modify StudentOrderPaymentResultResponse.paymentType

--- src/main/kotlin/com/cn/langujet/actor/order/payload/StudentOrderPaymentResultResponse.kt
+++ src/main/kotlin/com/cn/langujet/actor/order/payload/StudentOrderPaymentResultResponse.kt
@@ -2,19 +2,18 @@ package com.cn.langujet.actor.order.payload
 
 import com.cn.langujet.domain.order.model.OrderEntity
 import com.cn.langujet.domain.order.model.OrderStatus
-import com.cn.langujet.domain.payment.model.PaymentType
 import java.util.*
 
 data class StudentOrderPaymentResultResponse(
     val status: OrderStatus,
-    val paymentType: PaymentType?,
+    val paymentType: String?,
     val totalPrice: Double,
     val finalPrice: Double,
     val date: Date,
 ) {
     constructor(orderEntity: OrderEntity) : this(
         orderEntity.status,
-        orderEntity.paymentType,
+        orderEntity.paymentType?.displayName,
         orderEntity.totalPrice,
         orderEntity.finalPrice,
         orderEntity.date
