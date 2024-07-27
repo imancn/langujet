0082abf08e535dbb59a81f5c5923d442610be70d
Iman Soltani <iman.soltani.j@gmail.com>
2024-07-27 03:49:40 +0330

    Modify PaymentType icon links

--- src/main/kotlin/com/cn/langujet/domain/payment/model/PaymentType.kt
+++ src/main/kotlin/com/cn/langujet/domain/payment/model/PaymentType.kt
@@ -1,6 +1,6 @@
 package com.cn.langujet.domain.payment.model
 
 enum class PaymentType(var displayName: String, val icon: String) {
-    STRIPE("Stripe", "https://storage.langujet.com/staticfiles/Icons/payments/stripe-logo.svg"),
-    ZARIN_PAL("زرین پال", "https://storage.langujet.com/staticfiles/Icons/payments/zarinpal-logo.svg"),
+    STRIPE("Stripe", "https://storage.langujet.com/static-files/icons/payments/stripe-logo.svg"),
+    ZARIN_PAL("زرین پال", "https://storage.langujet.com/static-files/icons/payments/zarinpal-logo.svg"),
 }
