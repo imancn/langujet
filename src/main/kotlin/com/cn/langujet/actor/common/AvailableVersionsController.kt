41e4cd59ac3eeac64135634d90b09d3ce84e5671
Iman Soltani <iman.soltani.j@gmail.com>
2024-08-12 01:44:45 +0330

    Modify MinVersionController

--- src/main/kotlin/com/cn/langujet/actor/common/AvailableVersionsController.kt
+++ src/main/kotlin/com/cn/langujet/actor/common/AvailableVersionsController.kt
@@ -1,6 +1,5 @@
 package com.cn.langujet.actor.common
 
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,8 +7,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/student/mobile-app/available-versions")
 class AvailableVersionsController {
-    
-    @PreAuthorize("hasRole('STUDENT')")
     @GetMapping
     fun getMobileAppAvailableVersions(): MobileAppAvailableVersions {
         return MobileAppAvailableVersions(
